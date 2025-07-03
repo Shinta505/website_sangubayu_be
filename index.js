f83a080f222187da454384ad0cc8186cb8b45f29
Shinta Nursobah Chairani <shinta505@gmail.com>
2025-07-03 22:58:54 +0700

    tambah api dokumentasi

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import UmkmRoute from './routes/UmkmRoute.js';
 import ProdukRoute from './routes/ProdukRoute.js';
+
+// Konfigurasi __dirname untuk ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
+// Atur view engine ke EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 // Middleware
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
-app.use('/api/', UmkmRoute);
-app.use('/api/', ProdukRoute);
+// Rute untuk menampilkan dokumentasi API
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// Routes API
+app.use('/api', UmkmRoute);
+app.use('/api', ProdukRoute);
+
 
 // Server
 app.listen(PORT, '0.0.0.0', () => {

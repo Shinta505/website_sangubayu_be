6239d541f686dba9455b909d6d3b59c434717a18
Shinta Nursobah Chairani <shinta.nur505@gmail.com>
2025-07-03 23:09:11 +0700

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,18 +1,20 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import pg from 'pg';
 
 dotenv.config();
 
 const db = new Sequelize(process.env.POSTGRES_URL, {
   dialect: "postgres",
   protocol: "postgres",
+  dialectModule: pg, // <-- Tambahkan baris ini
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false // Necessary for Vercel Postgres/Heroku
+      rejectUnauthorized: false
     }
   },
-  logging: false // Optional: set to console.log to see SQL queries
+  logging: false
 });
 
 export default db;

aae51d7e67cf0aa2faf1e5dd7ce7f862a6714d7b
Shinta Nursobah Chairani <shinta505@gmail.com>
2025-07-08 18:11:01 +0700

    ubah codingan raja ongkir controller

--- controllers/RajaOngkirController.js
+++ controllers/RajaOngkirController.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import querystring from 'querystring';
 
 // Fungsi untuk menghitung biaya pengiriman
 async function calculateCost(req, res) {
@@ -10,12 +11,12 @@ async function calculateCost(req, res) {
     }
 
     try {
-        const response = await axios.post('https://api.rajaongkir.com/starter/cost', {
+        const response = await axios.post('https://api.rajaongkir.com/starter/cost', querystring.stringify({
             origin,
             destination,
             weight,
             courier
-        }, {
+        }), {
             headers: {
                 key: process.env.RAJAONGKIR_API_KEY,
                 'content-type': 'application/x-www-form-urlencoded'
@@ -23,7 +24,7 @@ async function calculateCost(req, res) {
         });
         res.status(200).json(response.data);
     } catch (error) {
-        console.error("Error calculating shipping cost:", error.message);
+        console.error("Error calculating shipping cost:", error.response ? error.response.data : error.message);
         res.status(500).json({ msg: "Terjadi kesalahan pada server saat menghitung biaya pengiriman." });
     }
 }
